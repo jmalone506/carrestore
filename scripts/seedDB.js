43469f1b3412869c09cb95a584eee5461492d347
Roseanne Nguyen <rtnguye1@gmail.com>
2021-04-14 19:49:38 -0700

    deploying to test mongo atlas on heroku

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -4,9 +4,19 @@ const db = require("../models");
 // This file empties the Books collection and inserts the books below
 
 mongoose.connect(
-    process.env.MONGODB_URI ||
-    "mongodb://localhost/reactreadinglist"
+    process.env.MONGODB_URI || 'mongodb://localhost/carsdb',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
 );
+
+// mongoose.connect(
+//     process.env.MONGODB_URI ||
+//     "mongodb://localhost/reactreadinglist"
+// );
 
 const bookSeed = [
     {
