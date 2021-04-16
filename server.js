b7363f4c181832b5eb9602ce29895b35e8c77a7a
Roseanne Nguyen <rtnguye1@gmail.com>
2021-04-15 19:43:53 -0700

    updated mongose connect with new mongo db

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+    process.env.MONGODB_URI || "mongodb://localhost/carsdb"
 );
 
 // Start the API server
