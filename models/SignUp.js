ead5d016383e065892de88ffb1eba1a3dbc626eb
Roseanne Nguyen <rtnguye1@gmail.com>
2021-04-20 22:44:55 -0700

    sign up isn't work yet. still configuring

--- models/SignUp.js
+++ models/SignUp.js
@@ -16,13 +16,6 @@ const userSchema = mongoose.Schema({
     },
 });
 
-userSchema.methods.generateHash = function(password){
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-userSchema.methods.validPassword = function (password) {
-    return bcrypt.compareSync(password, this.password);
-};
 
 
 export default mongoose.model("User", userSchema);
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/carsdb',
     {
