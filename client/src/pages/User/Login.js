b3308fee5f7fdd0ba52f2f58487d6c58a4b1a5b3
Roseanne Nguyen <rtnguye1@gmail.com>
2021-04-25 23:13:51 -0700

    set up login form

--- client/src/pages/User/Login.js
+++ client/src/pages/User/Login.js
@@ -1,96 +1,122 @@
 import React, { useEffect, useState } from "react";
-import Jumbotron from "../../components/Jumbotron";
 import { Col, Row, Container } from "../../components/Grid";
 // import { GoogleLogin } from 'react-google-login';
-import GoogleBtn from "../../components/GoogleBtn/"
+import GoogleBtn from "../../components/GoogleBtn"
 import Welcome from "../../components/Welcome"
-import loginIMG from "../../images/login.png"
+import loginIMG from "../../images/login.png";
+import "./style.css";
 
-function Login() {
-    // const [email, setEmail] = useState("");
-    // const [id, setId] = useState("");
-    // const [formObject, setFormObject] = useState({
-    // })
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.clearForm = this.clearForm.bind(this);
+    }
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
 
-    // const History = useHistory();
-
-
-    // function handleSubmit(event) {
-    //     event.preventDefault();
-    //     if (formObject.id && formObject.email) {
-    //         API.getUser({
-    //             id: formObject.id,
-    //             email: formObject.email,
-    //             password: formObject.password
-    //         })
-    //             .then(() => setFormObject({
-    //                 name: "",
-    //                 email: "",
-    //                 password: ""
-    //             }))
-    //             .then(() => {
-    //                 loadUser();
-    //                 History.push("/cars")
-    //             })
-
-    //             .catch(err => console.log(err));
-    //     }
-    // }
+        this.setState({
+            [name]: value
+        });
+    }
+    handleSubmit(event) {
+        event.preventDefault(event);
+        console.log(this.state);
+    }
+    clearForm() {
+        this.setState({
+            email: "",
+            password: ""
+        });
+    }
+    render() {
         return (
-            <Container fluid>
-                <Welcome></Welcome>
+            <div className="loginPage"><Welcome></Welcome>
                 <Row>
                     <Col size="md-12">
                         <img className="loginIMG" src={loginIMG} alt="login image.."></img>
                     </Col>
                 </Row>
 
+                <div className="container mt-5">
+                    <div className="row">
+                        <div className="col">
+                            <div className="card mx-auto">
+                                <div className="card-body">
+                                    <h1
+                                        className="card-title"
+                                        style={{ borderBottom: "1px solid #efefef" }}
+                                    >
+                                        Login Form
+                </h1>
+                                    <form
+                                        className="needs-validation"
+                                        noValidate
+                                        onSubmit={this.handleSubmit}
+                                    >
+                                        <div className="form-group">
+                                            <label htmlFor="exampleInputEmail1">Email address</label>
+                                            <input
+                                                type="email"
+                                                name="email"
+                                                className="form-control"
+                                                id="exampleInputEmail1"
+                                                aria-describedby="emailHelp"
+                                                required
+                                                placeholder="Enter email"
+                                                value={this.state.email}
+                                                onChange={this.handleChange}
+                                            />
+                                            <small id="emailHelp" className="form-text text-muted">
+                                                We'll never share your email with anyone else.
+          </small>
+                                        </div>
+                                        <div className="form-group">
+                                            <label htmlFor="exampleInputPassword1">Password</label>
+                                            <input
+                                                type="password"
+                                                name="password"
+                                                className="form-control"
+                                                id="exampleInputPassword1"
+                                                required
+                                                placeholder="Password"
+                                                value={this.state.password}
+                                                onChange={this.handleChange}
+                                            />
+                                        </div>
+                                        <button type="submit" className="btn btn-primary">
+                                            Login
+        </button>
+                                        <button
+                                            type="button"
+                                            className="btn btn-secondary float-right"
+                                            onClick={this.clearForm}
+                                        >
+                                            Clear
+        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+</div>
+                 
 
-                <Row>
-                    <Col size="md-3 p-3">
-
-                    </Col>
-
-                    <Col size="md-6 sm-12">
-
-                        <h1>Login</h1>
-                        <br></br>
-                        <GoogleBtn>Login Through Google</GoogleBtn>
-                        <br></br>
-                        <p>or</p>
-                        <form>
-
-                            {/* <Input
-                                autoFocus
-                                type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                            <Input
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
 
 
-
-                            <FormBtn
-
-                                onClick={handleSubmit}
-                            >
-                                Login
-              </FormBtn> */}
-                        </form>
-
-                    </Col>
-                    <Col size="md-3">
-
-
-                    </Col>
-                </Row>
-            </Container>
+                </div>
         );
+    }
 }
+
+
 
 
 export default Login
