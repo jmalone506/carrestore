1ce5df9a6ec422cf0ffea26bc2f92109136d1732
Roseanne Nguyen <rtnguye1@gmail.com>
2021-05-01 07:43:30 -0700

    created card component

---
+++ client/src/components/Cards/Cards.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import carguru from "../../images/carguru.jpg"
+
+
+const Cards = () => {
+    return (
+        <div className="card text-center">
+            <div className="overflow">
+                <img src={carguru} alt="" />
+            </div>
+        </div>
+
+    )
+}
+
+export default Cards
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import register from './serviceWorker';
 import App from "./App";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
     <BrowserRouter>
