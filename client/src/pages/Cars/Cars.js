9af75a3f85b5ec4d3672d5d3148da7e0b745b482
Roseanne Nguyen <rtnguye1@gmail.com>
2021-05-02 15:40:10 -0700

    working on styling with alicia and jammie

--- client/src/pages/Cars/Cars.js
+++ client/src/pages/Cars/Cars.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { Col, Row, Container } from "../../components/Grid";
+
 import Navbar from "../../components/Navbar";
 import CarouselIMG from "../../components/Carousel/Carousel"
-import HomeSection1 from "../../components/HomeSection1/"
+import HomeSection1 from "../../components/Section/HomeSection1"
 
 
 export class Cars extends Component {
