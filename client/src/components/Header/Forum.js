6440d75a4f943a03e5672cc9e6c3f8b13fa904f9
Roseanne Nguyen <rtnguye1@gmail.com>
2021-05-02 16:37:31 -0700

    Forum page completed. Added image and text that page is under construction for future develpments

---
+++ client/src/components/Header/Forum.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+import forumIMG from "../../images/forum.png";
+
+
+function Forum( ) {
+  return (
+    
+    <div className="construction">
+      
+
+      <img className="forumIMG" src={forumIMG} alt="page under construction" ></img>
+
+      <h1 className="forumTitle" src="./cars">
+        
+          Page Under Construction
+             
+      </h1>
+
+    </div>
+  );
+}
+
+export default Forum;
--- client/src/pages/Cars/Forum.js
+++ client/src/pages/Cars/Forum.js
@@ -1,35 +1,23 @@
 import Jumbotron from "../../components/Jumbotron";
 import { Col, Row, Container } from "../../components/Grid";
 import Navbar from "../../components/Navbar";
+import ForumIMG from "../../components/Header/Forum.js"
 
 function Forum() {
-   
+
 
     return (
         <Container fluid>
 
-            <Navbar></Navbar>
+            <Navbar />
+
             <Row>
                 <Col size="md-12">
-                    <h1>Forum </h1>
-                    <Jumbotron>
-                        <h1>Image</h1>
-                    </Jumbotron>
+                    < ForumIMG />
                 </Col>
             </Row>
 
 
-            <Row>
-                <Col size="md-8">
-                    <Jumbotron>
-                        <h1>About Car Hacks</h1>
-                    </Jumbotron>
-
-                </Col>
-                <Col size="md-4 sm-12">
-
-                </Col>
-            </Row>
         </Container>
     );
 }
